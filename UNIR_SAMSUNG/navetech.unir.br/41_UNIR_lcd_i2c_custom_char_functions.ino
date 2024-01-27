2e8e2e51da927cec52ef9972086c43db9cfef318
Gilberto Oliveira Jr <giljr.2009@gmail.com>
2024-01-27 17:34:16 -0400

    Update 41_UNIR_lcd_i2c_custom_char_functions.ino

--- UNIR_SAMSUNG/navetech.unir.br/41_UNIR_lcd_i2c_custom_char_functions.ino
+++ UNIR_SAMSUNG/navetech.unir.br/41_UNIR_lcd_i2c_custom_char_functions.ino
@@ -46,15 +46,17 @@ it under the terms of the GNU General Public License v3 as published by
 LCD_I2C lcd(0x27, 16, 2);
 
 
-uint8_t solar[8]  = {0b11111,0b10101,0b11111,0b10101,0b11111,0b10101,0b11111,0b00000};
-uint8_t battery[8]  = {0b01110,0b11011,0b10001,0b10001,0b10001,0b10001,0b10001,0b11111};
-uint8_t termometer[8]  = {0b00100,0b01010,0b01010,0b01110,0b01110,0b11111,0b11111,0b01110};
-uint8_t humidity[8]  = {0b00100,0b01010,0b01010,0b01110,0b00100,0b00000,0b00100,0b00000};
+uint8_t solar[8] = {0b11111,0b10101,0b11111,0b10101,0b11111,0b10101,0b11111,0b00000};
+uint8_t battery[8] = {0b01110,0b11011,0b10001,0b10001,0b10001,0b10001,0b10001,0b11111};
+uint8_t termometer[8] = {0b00100,0b01010,0b01010,0b01110,0b01110,0b11111,0b11111,0b01110};
+uint8_t humidity[8] = {0b00100,0b01010,0b01010,0b01110,0b00100,0b00000,0b00100,0b00000};
 uint8_t current[8] = {0b00001,0b00010,0b00100,0b01000,0b11000,0b00110,0b00100,0b01000};
-uint8_t charge[8]  = {0b01010,0b11111,0b10001,0b10001,0b10001,0b01110,0b00100,0b00100};
+uint8_t charge[8] = {0b01010,0b11111,0b10001,0b10001,0b10001,0b01110,0b00100,0b00100};
 uint8_t alarm[8] = {0b00000,0b00100,0b01110,0b01110,0b01110,0b11111,0b00000,0b00100};
-uint8_t degree[8] = { 0b00110,0b01001,0b01001,0b00110,0b00000,0b00000,0b00000,000000,};
-uint8_t _not[8] = {0b00000,0b10001,0b01010,0b00100,0b01010,0b10001,0b00000,0b00000};
+uint8_t full_battery[8] = {0b01110,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111,0b11111};
+// uint8_t negate[8] = {0b00000,0b10001,0b01010,0b00100,0b01010,0b10001,0b00000,0b00000};
+// uint8_t degree[8] = { 0b00110,0b01001,0b01001,0b00110,0b00000,0b00000,0b00000,000000,};
+
 void setup()
 {
     lcd.begin();
@@ -68,17 +70,7 @@ void setup()
     lcd.createChar(4, current);
     lcd.createChar(5, charge);
     lcd.createChar(6, alarm);
-    lcd.createChar(7, degree);
-
-
-    // lcd.write(0);
-    // lcd.write(1);
-    // lcd.write(2);
-    // lcd.write(3);
-    // lcd.write(4);
-    // lcd.write(5);
-    // lcd.write(6);
-    // lcd.write(7);
+    lcd.createChar(7, full_battery);
 
     lcd.setCursor(0, 0);
     // Print a message to the LCD.
@@ -87,97 +79,65 @@ void setup()
     lcd.print("     Characters ");
     delay(2000);
 
+}
 
+/*
+ Displays a custom character along with a description, value, and unit on the LCD.
+*/
+void displaySymbol(int symbol, const char* description, char* value, char* unit) {
+    
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(" ");
+    lcd.print(description);
+    lcd.setCursor(0, 1);
+    lcd.write(symbol);
+    lcd.setCursor(2, 1);
+    lcd.print(value);
+    lcd.print(unit);
+    delay(3000);
+}
+/*
+  Prints a custom character along with a value and unit at a specified position on the LCD.
+*/
+void printMosaic(int column, int row, uint8_t symbol, char* value, char* unit) {
+    lcd.setCursor(column, row);
+    lcd.write(symbol);
+    lcd.setCursor(column+2, row);    
+    lcd.print(value);
+    lcd.print(unit);
 }
 
 void loop()
 {
-
-  // Present each symbol
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(" Solar Grid");
-  lcd.setCursor(0, 1);
-  lcd.write(0);  // solar grid
-  lcd.setCursor(2, 1);
-  lcd.print("*"); 
-  delay(3000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(" Battery Status");
-  lcd.setCursor(0, 1);
-  lcd.write(1);  // battery status
-  lcd.setCursor(2, 1);
-  lcd.print("*");
-  delay(3000);
-
-  lcd.clear();
-  lcd.setCursor(0, 0);  
-  lcd.print(" Temperature (C)");
-  lcd.setCursor(0, 1);
-  lcd.write(2);
-  lcd.setCursor(2, 1);
-  lcd.print("*");   //lcd.write(7); lcd.print("C"); 
-  delay(3000);
-
-  lcd.clear();
-  lcd.setCursor(0, 0);  
-  lcd.print(" Humidity (%)");
-  lcd.setCursor(0, 1);
-  lcd.write(3);
-  lcd.setCursor(2, 1);
-  lcd.print("*");   //lcd.write(6); lcd.print(" C "); 
-  delay(3000);
-
-  lcd.clear();
-  lcd.setCursor(0, 0);  
-  lcd.print(" Current(A)");
-  lcd.setCursor(0, 1);
-  lcd.write(4);
-  lcd.setCursor(2, 1);
-  lcd.print("*");   //lcd.write(6); lcd.print(" C "); 
-  delay(3000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);  
-  lcd.print(" Charging");
-  lcd.setCursor(0, 1);
-  lcd.write(5);
-  lcd.setCursor(2, 1);
-  lcd.print("*");   //lcd.write(6); lcd.print(" C "); 
-  delay(3000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);  
-  lcd.print(" Alarm ");
-  lcd.setCursor(0, 1);
-  lcd.write(6);
-  lcd.setCursor(2, 1);
-  lcd.print("*");   //lcd.write(6); lcd.print(" C "); 
-  delay(3000);
-  
-  // mosaic
-  lcd.clear();
-
-  lcd.setCursor(0, 0);
-  lcd.write(0);
-  lcd.setCursor(2, 0);
-  lcd.print("*");
-
-  lcd.setCursor(8, 0);
-  lcd.write(0);
-  lcd.setCursor(10, 0);
-  lcd.print("*");
+    // Presentation: 
+    // Sequentially showcasing each symbol along with a descriptive explanation of its significance.
+    displaySymbol(0, "Solar Grid", "12.2", "v");
+    displaySymbol(1, "Battery Status", "10.1", "v");
+    displaySymbol(2, "Temperature (C)", "12.2", "c");
+    displaySymbol(3, "Humidity (%)", "10.1", "%");
+    displaySymbol(4, "Current(A)", "12.2", "a");
+    displaySymbol(5, "Charging", "10.1", "v");
     
-  lcd.setCursor(0, 1);
-  lcd.write(0);
-  lcd.setCursor(2, 1);
-  lcd.print("*");
 
-  lcd.setCursor(8, 1);
-  lcd.write(0);
-  lcd.setCursor(10, 1);
-  lcd.print("*");
-  delay(3000);
+
+   // Mosaic: 
+   // A visual arrangement formed by combining the symbol and its corresponding value;
+   // This technique maximizes information display within a compact area like an LCD 16x2.
+    lcd.clear();
+    printMosaic(0, 0, 0, "12.2", "v");
+    printMosaic(8, 0, 1, "10.1", "v");
+    printMosaic(0, 1, 2, "10.1", "C" );
+    printMosaic(8, 1, 3, "10.1", "%");
+
+    delay(6000);
+    lcd.clear();
+
+    printMosaic(0, 0, 4, "4.7", "a");
+    printMosaic(8, 0, 5, "10.1", "v");
+    printMosaic(0, 1, 6, "Off", "  " );
+    printMosaic(8, 1, 7, " full ", " - ");
+    delay(6000);
+
 }
+
